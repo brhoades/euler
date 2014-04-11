6349fe4c7f4c7011646cccd7114d2b531db80df8
Billy Rhoades <billyarhoades@gmail.com>
2014-04-10 23:45:30 -0500

    Now returns a massive list with all the n's from 0-nmax followed by their collatz.

--- runner.lisp
+++ runner.lisp
@@ -1,9 +1,20 @@
-(defun longest_collatz (num)
+(defun longest_collatz (nmax)
+  (progn
+    (setf colz (list 0 0))
+    (loop for i from 1 upto nmax do
+      (setf colz (append colz (list i (collatz i)))
+    ))
+    (return-from longest_collatz colz)
+  )
+)
+
+
+(defun collatz (num)
   (progn
     (setf cnt 0)
     (loop
       (if (= num 1)
-        (return-from longest_collatz cnt)
+        (return-from collatz cnt)
         (progn
           (if (= (mod num 2) 0)
               (setf num (/ num 2))
@@ -15,8 +26,10 @@
     )
   )
 )
+
+
 (progn
-  (print (longest_collatz 5))
+  (print (longest_collatz 100))
   (quit)
 )
 
